488cf8d2f690cf94b17f9ff070fa0775b7680936
Huseyin Akten <husakten@gmail.com>
2020-01-25 13:44:15 -0600

    asdf

--- security_group.tf
+++ security_group.tf
@@ -42,3 +42,22 @@ ingress {
     cidr_blocks     = ["0.0.0.0/0"] 
   } 
 }
+
+resource "aws_security_group" "db" { 
+  name        = "db" 
+  description = "Allow db inbound traffic" 
+  vpc_id      = module.vpc.vpc_id
+
+  ingress { 
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp" 
+    cidr_blocks = ["0.0.0.0/0"]   
+} 
+  egress { 
+    from_port       = 0 
+    to_port         = 0 
+    protocol        = "-1" 
+    cidr_blocks     = ["0.0.0.0/0"] 
+  } 
+}
