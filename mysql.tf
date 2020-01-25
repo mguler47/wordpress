22a23900946465369dc1be1b236f488e07428a93
Huseyin Akten <husakten@gmail.com>
2020-01-25 13:48:16 -0600

    adf

--- mysql.tf
+++ mysql.tf
@@ -16,7 +16,7 @@ resource "aws_db_instance" "rds" {
 
 resource "aws_db_subnet_group" "default" {
   name       = "main"
-  subnet_ids = [module.vpc.private_subnets[0]]
+  subnet_ids = [module.vpc.private_subnets[0],module.vpc.private_subnets[1]]
 
   tags = {
     Name = "My DB subnet group"
