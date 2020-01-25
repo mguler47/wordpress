819c79687bdcf7b069b1b5be232685eda9dbb3aa
Huseyin Akten <husakten@gmail.com>
2020-01-25 16:16:21 -0600

    sadfa

--- mysql.tf
+++ mysql.tf
@@ -15,7 +15,7 @@ resource "aws_db_instance" "rds" {
 }
 
 resource "aws_db_subnet_group" "db_subnet" {
-  subnet_ids = ["${aws_vpc.private_subnets[0]}","${aws_vpc.private_subnets[1]}"]
+  subnet_ids = ["${aws_vpc.main.private_subnets[0]}","${aws_vpc.main.private_subnets[1]}"]
 
   tags = {
     Name = "${var.environment}"
