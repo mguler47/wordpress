79821526907b21e6a36fa5ca10e0a1891d70b3a7
Huseyin Akten <husakten@gmail.com>
2020-01-26 01:11:01 -0600

    deleted everything

--- backends/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    region = "eu-west-1"
-    bucket = "t-s3-backend"
-    key    = "wordpress/arch/infrastructure.tfstate"
-  }
-}
--- key.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_key_pair" "bastion-wordpress" { 
-  key_name   = "bastion-wordpress" 
-  public_key = file("~/.ssh/id_rsa.pub")
-} 
