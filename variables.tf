61ecdd7ff94d382fb09abe4a710053199a6ad4da
Huseyin Akten <husakten@gmail.com>
2020-01-26 14:44:14 -0600

    key added

--- variables.tf
+++ variables.tf
@@ -1,13 +1,3 @@
 variable "region" {}
 
-variable "cidr" {}
-variable "project" {}
-variable "environment" {}
-
-variable "azs" {}
-variable "public_subnets" {}
-variable "private_subnets" {}
-
-
-variable "instance_type" {}
 
